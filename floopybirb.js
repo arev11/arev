611e0f388f6e103366160769f3e245b86281f3ae
arev11 <74614760+arev11@users.noreply.github.com>
2021-01-13 12:57:34 -0500

    HW check 1: Jan 13

--- floopybirb.js
+++ floopybirb.js
@@ -2,23 +2,23 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-var x = myCanvas.width / 20; //These 2 variables determine the starting circles location, in this case, the top left of the screen.
-var y = myCanvas.height / 20;
-var x = myCanvas.width / 2; //These 2 variables determine the starting circles location, in this case, the middle of the screen.
+var x = myCanvas.width / 2; //These 2 variables set the starting circles location.
 var y = myCanvas.height / 2;
+var x = myCanvas.width / 20; //These 2 variables set the starting circles location.
+var y = myCanvas.height / 20;
 
-var dx = 10; //These variables will be used later to change the position of the circle.
-var dy = 10; //Changing both of these numbers will also change the speed of the circle (in other words, how many units the circle moves per frame).
+var dx = 10; //dx and dy variables will be used later to change the position of the circle.
+var dy = 10; //Changing both of these numbers will also change the speed of the circle.
 
 
 
-// @function exeptionsRand();
-// @param exeption [integer] : telles the function when to start excluding
-// @param sign [string] {Expects} ["<",">","<=",">="]: tells the function what signs to use
-// @param inOrOut [string] {Expects} ["in","out"]: Tells the function weather or not to use out or in for the ifs
-// @param max [integer] {requires} [max > min] : tells the function when to stop;
-// @param min [integer] {requires} [min < max] : tells the function when to start;
-function exeptionsRand(exeption, sign, max,min) {
+
+
+
+
+
+
+function exeptionsRand(exeption, sign, max,min) {// function exeptionsRand();
     var randomInitial = Math.floor(Math.random()*max);
     var acceptable = false;
     while (acceptable == false) {
@@ -29,13 +29,13 @@ function exeptionsRand(exeption, sign, max,min) {
           randomInitial = Math.floor(Math.random()*max);
         }
     }else if (sign == "<") {
-        if (randomInitial > exeption) {
+        if (randomInitial > exeption) {// param exeption [integer] : telles the function when to start excluding
             acceptable = true;
         }else{
           randomInitial = Math.floor(Math.random()*max);
         }
     }
-    else if (sign == "<=") {
+    else if (sign == "<=") {// param sign [string] {Expects} ["<",">","<=",">="]: tells the function what signs to use
         if (randomInitial >= exeption) {
             acceptable = true;
         }else{
@@ -56,14 +56,14 @@ function exeptionsRand(intOrOut,max,min) {
     var inBool = true;
   while (inBool == true) {
   var randNumber = Math.floor(Math.random()*(max-min))+min;
-  if (intOrOut == "in") {
+  if (intOrOut == "in") {// param inOrOut [string] {Expects} ["in","out"]: Tells the function weather or not to use out or in for the ifs
     if (max >= randNumber && randNumber >= min) {
         inBool = false;
     }else{
-    randNumber = Math.floor(Math.random()*(max-min)) + min;
+    randNumber = Math.floor(Math.random()*(max-min)) + min;// param min [integer] {requires} [min < max] : tells the function when to start;
     }
   }else if (intOrOut == "out") {
-    if (min >= randNumber <= max) {
+    if (min >= randNumber <= max) {// param max [integer] {requires} [max > min] : tells the function when to stop;
         inBool = false;
     }else{
           randNumber = Math.floor(Math.random()*max)-min;
@@ -75,12 +75,13 @@ function exeptionsRand(intOrOut,max,min) {
   }
   return randNumber;
 }
+        //pipes functions
 var pipx = exeptionsRand(15, "<",myCanvas.width-15,0);
 var pipy = exeptionsRand(15, "<",Math.floor(myCanvas.height/3)-15,0);
-var pipeW1 = exeptionsRand(28, ">", 50,0);
+var pipeW1 = exeptionsRand(28, ">", 50,0);//exceptions
 var pipeH1 = exeptionsRand(36, ">", 2456,0);
 var pipBotObj = {width:pipeW1,height:234, x:pipx ,y:pipy+460,gap: 20};
-var pipTopObj = {width: pipeW1,height:pipBotObj.gap, x:pipx,y:0};
+var pipTopObj = {width: pipeW1,height:pipBotObj.gap, x:pipx,y:0};//walls to each pipe
 var pipeWalls = {north:undefined,east:undefined,south:undefined,west:undefined};
 var pipx = exeptionsRand("in", myCanvas.width-30, 75);
 var pipy = exeptionsRand("in", myCanvas.height-35,90);
@@ -88,12 +89,12 @@ var pipBotObj = {width:45,height:450, x:pipx ,y:pipy,gap: 40};
 var pipTopObj = {width: 45,height:pipy-(pipBotObj.gap*2), x:pipx,y: 0};
 
 
-var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
-var damping = 0.75; //The rate at which the ball slows down.
-var damping = 0.01; //The rate at which the ball slows down.
-var traction = 0.95; //Will make the ball stop.
+var gravity = 0.2; // gravity pulling the ball to the ground.
+var damping = 0.75; //the ball slows down.
+var damping = 0.01; //ball slows down.
+var traction = 0.95; //makes the ball stop.
 var ballSize = 20; //Sets the circle's radius.
-function drawPipes(){
+function drawPipes(){//draw pipes
     ctx.beginPath();
     ctx.rect(pipBotObj.width, pipBotObj.height, pipBotObj.x,pipBotObj.y);
     ctx.rect(pipTopObj.width, pipTopObj.height, pipTopObj.x, pipTopObj.y);
@@ -114,33 +115,32 @@ function drawPipes(){
 
 
 
-function drawCircle() {
+function drawCircle() {//draw circle
   ctx.beginPath();
-  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle, on frame one, will always start at the top left, and its size will always be set to ballSize.
-  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle, on frame one, will always start in the middle, and its size will always be set to ballSize.
+  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle will always start at the top left, and  size will always be set to ballSize.
+  ctx.arc(x, y, ballSize, 0, Math.PI*2); //This circle will always start in the middle, and its size will always be set to ballSize.
   ctx.fillStyle = "#0095DD"; //Sets the color of the circle to light blue.
-  ctx.fill(); //Fills in the circle with the color provided in fillStyle.
+  ctx.fill(); //Fills in the circle with the color in fillStyle.
   ctx.stroke();
-@@ -97,23 +89,24 @@ drawPipes();
+-97,23 +89,24 @@ drawPipes();
 
-  dy += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
-
+  dy += gravity; //Adds gravity value to the ball's dy value.
   x += dx;
   //x += dx;
 
   if (((y + dy) + ballSize) <= 300) {
-    y += dy;
+    y += dy;//y += dy;
   }
 }
 
-setInterval(draw, 10);
-document.addEventListener("keypress", keyPress);
-function keyPress(e) {
+setInterval(draw, 10);// set timer function
+document.addEventListener("keypress", keyPress);// keypress
+function keyPress(e) {// e key press 
     if (e.key == " ") {
 
-document.addEventListener("keypress", keyPress); //This will look for a key that is pressed.
+document.addEventListener("keypress", keyPress); //This will look for a specific key that is pressed.
 function keyPress(e) { //Function that will play out when a key is pressed (e is just a placeholder)
-    if (e.key == " ") { //When this key is pressed (the empty string represents the spacebar)
+    if (e.key == " ") { //When this key is pressed it will act as space
       //if (dx > 0) {
       //  dx+=5;
       //}
@@ -148,6 +148,6 @@ function keyPress(e) { //Function that will play out when a key is pressed (e is
       //  dx-=5;
       //}
       dy-=10;
-      dy-=10; //Will make the ball jump a small distance.
+      dy-=10; //Will make the ball jump a distance of 10 units.
     }
 }
